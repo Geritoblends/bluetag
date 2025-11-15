ad4c4dd595f8ce1e3468615d86efbe2540c39b0b
gero <geronimosotelolopez@gmail.com>
2025-11-14 22:14:48 -0600

    bruh

--- aws/database/main.sql
+++ aws/database/main.sql
@@ -3,39 +3,31 @@ USE bluetag;
 
 CREATE TABLE Tags (
     id INT NOT NULL AUTO_INCREMENT,
-    mac_address INT,
+    user_id INT NOT NULL,
+    mac_address VARCHAR(17) NOT NULL,
     last_distance VARCHAR(45),
-    last_date DATE,
-    PRIMARY KEY(id)
+    alias VARCHAR(45),
+    icon VARCHAR(45),
+    last_date DATETIME,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_Users_Tags FOREIGN KEY (user_id) REFERENCES Users(id)
 );
 
 CREATE TABLE Users (
     id INT NOT NULL AUTO_INCREMENT,
     name varchar(100),
+    email varchar(255),
     bcrypt varchar(255),
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    UNIQUE(email)
 );
 
-CREATE TABLE UserTag (
-    user_id INT NOT NULL,
-    alias VARCHAR(45),
-    icon VARCHAR(45),
+CREATE TABLE Updates (
+    id INT NOT NULL
     tag_id INT NOT NULL,
-    CONSTRAINT fk_Users_UserTag
-    FOREIGN KEY(user_id) references Users(id),
-    CONSTRAINT fk_Tags_UserTag
-    FOREIGN KEY(tag_id) references Tags(id)
-);
-
-/* CREATE TABLE RefreshTokens (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  user_id INT NOT NULL,
-  token VARCHAR(512) NOT NULL,
-  expires_at DATETIME NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  last_used TIMESTAMP NULL,
-  device_info VARCHAR(255),  
-  CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE,
-  UNIQUE KEY (token)
-); */
+    distance_to_router FLOAT,
+    time DATE,
+    PRIMARY KEY (id),
+    UNIQUE (tag_id, time)
+)
 
